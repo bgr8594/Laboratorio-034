9f7a79acb176aecc289dd0d14868e2d64c2b344a
Alan Santiago <alantadeo@live.com>
2021-09-25 10:12:09 -0500

    Practica 08
    Imports en Login y menu-flotante

--- ionic-basic/src/app/login/login.page.ts
+++ ionic-basic/src/app/login/login.page.ts
@@ -4,6 +4,7 @@ import { ModalController } from '@ionic/angular'; //Aun no visto
 import { ModalErrorComponent } from '../modal-error/modal-error.component'; //Aun no visto
 import { AuthService } from '../service/auth.service';
 import { User } from '../shared/user.class';
+import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +14,15 @@ import { User } from '../shared/user.class';
 export class LoginPage implements OnInit {
 
   user: User = new User();
+  ionicForm: FormGroup;
 
   constructor(private autSvc: AuthService,
     private router: Router, private modalCtrl: ModalController
+    ,private formBuilder: FormBuilder
     ) { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
   async onLogin(){
@@ -29,12 +33,13 @@ export class LoginPage implements OnInit {
     }
     else{
       if(user.code){
-        if(user.code=='auth/wrong-password' ||user.code=='auth/invalid-email' || user.code=='auth/argument-error'){
+        if(user.code=='auth/wrong-password' ||user.code=='auth/invalid-email' || user.code=='auth/user-not-found'){
           this.openModal(user);
         }
       }
     }
   }
+
   async openModal(user: any) {
     const modal = await this.modalCtrl.create({
       component: ModalErrorComponent,
@@ -44,5 +49,39 @@ export class LoginPage implements OnInit {
     });
     return await modal.present();
   }
+
+  submitForm(){
+    if(this.ionicForm.valid){
+      this.user.email=this.ionicForm.get('email').value;
+      this.user.password=this.ionicForm.get('password').value;
+      this.onLogin();
+    }
+
+  }
+
+  buildForm(){
+    this.ionicForm = this.formBuilder.group(
+      {email: new FormControl('',{validators:[Validators.email, Validators.required]}),
+      password: new FormControl('',{validators:[Validators.required, Validators.maxLength(6), Validators.minLength(6)]})
+    }
+      );
+  }
+
+  notZero(control: AbstractControl){
+    if(control.value && control.value.monto <= 0){
+      return{'notZero': true}
+    }
+    return null;
+  }
+
+  hasError: any = (controlName: string, errorName: string) => {
+    return !this.ionicForm.controls[controlName].valid &&
+    this.ionicForm.controls[controlName].hasError(errorName) &&
+    this.ionicForm.controls[controlName].touched;
+  }
+
+
+
+
 
 }
