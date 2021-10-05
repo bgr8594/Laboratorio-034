1bbf946fb4dcbeb103e8e626be83267fee4068cc
Benito <bgr8594@gmail.com>
2021-10-04 20:22:37 -0500

    Practica 10, consulta snashotChanges, edicion y eliminacion en firestore database, formgroup, validator en lugares

--- ionic-basic/src/app/destinos/destinos.page.ts
+++ ionic-basic/src/app/destinos/destinos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Lugar } from '../shared/lugar';
 import { LugaresService } from '../services/lugares.service';
+import {FormGroup, FormBuilder, Validators, FormControl, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-destinos',
@@ -10,15 +11,87 @@ export class DestinosPage implements OnInit {
 
   lugar: Lugar = new Lugar();
   destinos: any[] = [];
-  constructor(private lugaresService: LugaresService) { }
+  ionicForm: FormGroup;
+  estado: string ="Alta destino";
+  editando: boolean= false;
+  constructor(private lugaresService: LugaresService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.lugaresService.getLugares(this.destinos);
+    this.buildForm();
+    this.lugaresService.getLugaresChanges().subscribe(
+      response=>{
+        this.destinos = response.map((e:any)=>{
+          return {
+            id: e.payload.doc.id,
+            nombre: e.payload.doc.data().nombre
+          }
+        });
+       }, 
+      error=>{ console.error(error)}
+      )
   }
+
+
+  editarLugar(id: any, lugar: any) {
+    this.editando = true;
+    this.lugar = lugar;
+    this.estado = "Editar el lugar";
+    this.ionicForm.get('nombre').setValue(lugar.nombre);
+  }
+
+  eliminarLugar(id: any) {
+    this.estado = "Alta destino";
+    this.editando = false;
+    this.ionicForm.reset();
+    this.lugaresService.deleteLugar(id);
+  }
+
 
   altaLugar(){
     this.lugaresService.altaLugar(this.lugar);
-    this.lugaresService.getLugares(this.destinos);
   }
 
+  submitForm(){
+    if(this.ionicForm.valid){
+      this.lugar.nombre = this.ionicForm.get('nombre').value;
+      if(!this.editando){
+        this.lugaresService.altaLugar(this.lugar).then((e:any)=>{
+          this.ionicForm.reset();
+        }).catch(e=>{
+          console.error(e);
+        });        
+      } else{
+        this.lugaresService.updateLugares(this.lugar.id, this.lugar).then(e=>{
+          this.editando= false;
+          this.estado = "Alta destino";
+          this.lugar = new Lugar();
+          this.ionicForm.reset();
+        }).catch(e=>{
+          console.error(e);
+        });
+      }
+    }
+  }
+
+  cancelarEdicion(){
+    this.estado = "Alta destino";
+    this.editando = false;
+    this.ionicForm.reset();
+    this.lugar = new Lugar();
+  }
+
+  buildForm(){
+    this.ionicForm = this.formBuilder.group(
+      {
+        nombre: new FormControl('',{validators: [Validators.required]})
+      }
+    );
+  }
+
+  hasError: any = (controlName: string, errorName: string) => {
+		return !this.ionicForm.controls[controlName].valid &&
+			this.ionicForm.controls[controlName].hasError(errorName) &&
+			this.ionicForm.controls[controlName].touched;
+	}  
 }
