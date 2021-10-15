612df3591c997bbb0cde5e552b1565140752f71c
Benito <bgr8594@gmail.com>
2021-10-15 01:06:24 -0500

    Practica 11, implementar api, consulta GET, PUT, DELETE, POST desde ionic, uso de api  microservicio springboot

--- ionic-basic/src/app/destinos/destinos.page.ts
+++ ionic-basic/src/app/destinos/destinos.page.ts
@@ -1,37 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Lugar } from '../shared/lugar';
 import { LugaresService } from '../services/lugares.service';
 import {FormGroup, FormBuilder, Validators, FormControl, AbstractControl} from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-destinos',
   templateUrl: './destinos.page.html'
 })
-export class DestinosPage implements OnInit {
+export class DestinosPage implements OnInit, OnDestroy {
 
   lugar: Lugar = new Lugar();
   destinos: any[] = [];
   ionicForm: FormGroup;
   estado: string ="Alta destino";
   editando: boolean= false;
+  latitud : number;
+  longitud: number;
+  subscripcion: Subscription;
+
   constructor(private lugaresService: LugaresService,
     private formBuilder: FormBuilder) { }
+  ngOnDestroy(): void {
+    if(this.subscripcion){
+      this.subscripcion.unsubscribe();
+    }
+  }
 
   ngOnInit() {
+    this.getPosition();
     this.buildForm();
+    // consulta de lugares por medio de snapshoChanges que consulta en tiempo real
+    /*
     this.lugaresService.getLugaresChanges().subscribe(
       response=>{
         this.destinos = response.map((e:any)=>{
           return {
             id: e.payload.doc.id,
-            nombre: e.payload.doc.data().nombre
+            nombre: e.payload.doc.data().nombre,
+            latitud: e.payload.doc.data().latitud,
+            longitud: e.payload.doc.data().longitud
           }
         });
        }, 
       error=>{ console.error(error)}
-      )
+      );
+*/
+       // Llamada a consultar lugares por medio del api
+   this.subscripcion = this.getLugares();
+   
   }
 
+
+  getLugares(): Subscription{
+    return this.lugaresService.getLugaresApi().subscribe((response: Lugar[])=>{
+      this.destinos = response
+    }, error=>{
+      console.error();
+    });
+  }
 
   editarLugar(id: any, lugar: any) {
     this.editando = true;
@@ -41,10 +68,27 @@ export class DestinosPage implements OnInit {
   }
 
   eliminarLugar(id: any) {
-    this.estado = "Alta destino";
-    this.editando = false;
-    this.ionicForm.reset();
-    this.lugaresService.deleteLugar(id);
+    // Borrar lugar desde firestore
+    /*
+    this.lugaresService.deleteLugar(id).then(response=>{
+      this.estado = "Alta destino";
+      this.editando = false;
+      this.ionicForm.reset();
+    }).catch(error=>{
+      console.error(error);
+    });
+    */
+   // borrar lugar atraves de api-->firestore
+      this.lugaresService.borrarLugarApi(id).subscribe((response: any)=>{
+      if(response){
+        this.estado = "Alta destino";
+        this.editando = false;
+        this.ionicForm.reset();
+        this.subscripcion = this.getLugares();
+      }
+      }, error=>{
+      console.error(error);
+      });
   }
 
 
@@ -55,13 +99,27 @@ export class DestinosPage implements OnInit {
   submitForm(){
     if(this.ionicForm.valid){
       this.lugar.nombre = this.ionicForm.get('nombre').value;
+      this.lugar.latitud = this.latitud;
+      this.lugar.longitud = this.longitud; 
       if(!this.editando){
+/* alta lugar a traves de firestore
         this.lugaresService.altaLugar(this.lugar).then((e:any)=>{
           this.ionicForm.reset();
         }).catch(e=>{
           console.error(e);
         });        
+*/
+
+// alta de lugar desde api
+        this.lugaresService.altaLugarApi(this.lugar).subscribe((reponse: any)=>{
+          this.subscripcion = this.getLugares();
+          this.ionicForm.reset();
+        }, error=>{
+          console.log(error);
+        });
+
       } else{
+/* editar lugar a traves de firestore
         this.lugaresService.updateLugares(this.lugar.id, this.lugar).then(e=>{
           this.editando= false;
           this.estado = "Alta destino";
@@ -69,6 +127,18 @@ export class DestinosPage implements OnInit {
           this.ionicForm.reset();
         }).catch(e=>{
           console.error(e);
+        });
+*/
+
+//editar el lugar a traves de api-->firestore
+        this.lugaresService.editarLugarApi(this.lugar.id, this.lugar).subscribe((response: any)=>{
+          this.editando= false;
+          this.estado = "Alta destino";
+          this.lugar = new Lugar();
+          this.subscripcion = this.getLugares();
+          this.ionicForm.reset();
+        }, error=>{
+          console.error(error);
         });
       }
     }
@@ -93,5 +163,23 @@ export class DestinosPage implements OnInit {
 		return !this.ionicForm.controls[controlName].valid &&
 			this.ionicForm.controls[controlName].hasError(errorName) &&
 			this.ionicForm.controls[controlName].touched;
+	}  
+
+
+  getPosition(): Promise<any> {
+		return new Promise((resolve: any, reject: any): any => {
+			navigator.geolocation.getCurrentPosition((resp: any) => {
+				this.latitud = resp.coords.latitude;
+				this.longitud = resp.coords.longitude;
+			},
+			(err: any) => {
+				if ( err.code === 1 ) {
+					alert('Favor de activar la geolocalización en tu navegador y recargar la pantalla.');
+				}
+				this.latitud = null;
+				this.longitud = null;
+			}, {timeout: 5000, enableHighAccuracy: true });
+		});
+
 	}  
 }
