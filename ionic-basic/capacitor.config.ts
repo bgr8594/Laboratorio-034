05ce038547ede70fe161d36baad5bbd0aa582c10
Luis Roberto Carranza Espinoza <robert5j9n@gmail.com>
2021-11-13 23:38:16 -0600

    Practica 13

---
+++ ionic-basic/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionicbasic.fcfm',
+  appName: 'ionic-basic-2034235',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
