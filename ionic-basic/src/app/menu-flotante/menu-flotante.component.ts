5c8e336b85e260ccb15472bc1acafe768c9d3491
Alan Santiago <alantadeo@live.com>
2021-11-16 18:34:51 -0600

    Practica 15
    Ajustes de aplicación front

--- ionic-basic/src/app/menu-flotante/menu-flotante.component.ts
+++ ionic-basic/src/app/menu-flotante/menu-flotante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 import { MenuElement } from './menu.model';
 
 @Component({
@@ -9,27 +10,69 @@ import { MenuElement } from './menu.model';
 })
 export class MenuFlotanteComponent implements OnInit {
 
-  datosMenu: MenuElement[] =[
-    {nombre: 'Alumnos',enlace:'/alumnos',
-  icono:'school-outline'},
-    {nombre: 'Receteas',enlace:'/recetas',
-    icono:'restaurant-outline'},
-    {nombre: 'Tabs',enlace:'/tabs',
-    icono:'folder-outline'},
-    {nombre: 'Login',enlace:'/login',
-    icono:'home'},
-    {nombre: 'Destino',enlace:'/destinos',
-    icono:'train-outline'},
-    {nombre: 'Logout',enlace:'/home',
-    icono:'log-out-outline'} 
-  ];
+  datosMenu: MenuElement[] =[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private autSvc: AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if(this.autSvc.isLoged){
+      this.datosMenu = [
+        {nombre: 'Alumnos',enlace:'/alumnos',
+      icono:'school-outline'},
+        {nombre: 'Receteas',enlace:'/recetas',
+        icono:'restaurant-outline'},
+        {nombre: 'Tabs',enlace:'/tabs',
+        icono:'folder-outline'},
+        {nombre: 'Login',enlace:'/login',
+        icono:'home'},
+        {nombre: 'Destino',enlace:'/destinos',
+        icono:'train-outline'},
+        {nombre: 'Logout',enlace:'/home',
+        icono:'log-out-outline'} 
+      ];
+    } else{
+      this.datosMenu =
+      [
+          {nombre: 'register',enlace:'/register',
+          icono:'person-add'},
+          {nombre: 'login',enlace:'/login',
+          icono:'log-in'}
+        ];
+    }
+  }
 
   navegar(link: string){
     this.router.navigate([link]);
+  }
+
+  onMenuOpen(){
+    if(this.autSvc.isLoged){
+      this.datosMenu =
+      [
+        {nombre: 'Alumnos',enlace:'/alumnos',
+        icono:'school-outline'},
+          {nombre: 'Receteas',enlace:'/recetas',
+          icono:'restaurant-outline'},
+          {nombre: 'Tabs',enlace:'/tabs',
+          icono:'folder-outline'},
+          {nombre: 'register',enlace:'/register',
+          icono:'person-add'},
+          {nombre: 'login',enlace:'/login',
+          icono:'log-in'},
+          {nombre: 'Turismo',enlace:'/destinos',
+          icono:'airplane'},
+          {nombre: 'logout',enlace:'/home',
+          icono:'log-out'}
+        ];
+    } else{
+      this.datosMenu =
+      [
+          {nombre: 'register',enlace:'/register',
+          icono:'person-add'},
+          {nombre: 'login',enlace:'/login',
+          icono:'log-in'}
+        ];
+    }
   }
 
 }
